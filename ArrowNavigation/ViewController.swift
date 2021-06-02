1aefe87585f0cd0d14518d88241e8cef4d1a04f5
蘇健豪 <jenhausu@osensetech.com>
2021-06-02 10:42:06 +0800

    use SCNLookAtConstraint to always point to the target

--- ArrowNavigation/ViewController.swift
+++ ArrowNavigation/ViewController.swift
@@ -9,24 +9,44 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    private lazy var arrowNode: SCNNode = {
+        let scene = SCNScene(named: "art.scnassets/arrow.scn")!
+        let node = scene.rootNode
+        node.scale = SCNVector3(x: 0.5, y: 0.5, z: 0.5)
+        node.position = SCNVector3(x: 0, y: -1.5, z: -5)
+        
+        let constraint = SCNLookAtConstraint(target: targetNode)
+        node.constraints = [constraint]
+        
+        return node
+    }()
+    
+    private lazy var targetNode: SCNNode = {
+        let box = SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0)
+        let targetNode = SCNNode()
+        targetNode.geometry = box
+        targetNode.position = SCNVector3(x: -3, y: 0, z: -3)
+        
+        return targetNode
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
         // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene.rootNode.addChildNode(targetNode)
+        sceneView.pointOfView?.addChildNode(arrowNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,30 +65,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+}
 
-    // MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+}
+
+extension ViewController: ARSessionDelegate {
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
