b5562d4164c34ac6b9ca0f9f3ee479bd1d86e74c
蘇健豪 <jenhausu@osensetech.com>
2021-06-19 12:24:15 +0800

    feat: 2D transform

--- ArrowNavigation/ViewController.swift
+++ ArrowNavigation/ViewController.swift
@@ -29,7 +29,8 @@ class ViewController: UIViewController {
         let box = SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0)
         let targetNode = SCNNode()
         targetNode.geometry = box
-        targetNode.position = SCNVector3(x: -3, y: 0, z: -3)
+        
+        
         
         return targetNode
     }()
@@ -43,10 +44,6 @@ class ViewController: UIViewController {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Set the scene to the view
-        sceneView.scene.rootNode.addChildNode(targetNode)
-        sceneView.pointOfView?.addChildNode(arrowNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -67,6 +64,54 @@ class ViewController: UIViewController {
         sceneView.session.pause()
     }
     
+    // MARK: - Helper
+    
+    private func getPosition(from transform: simd_float4x4) -> SCNVector3 {
+        let position = transform.columns.3
+        return SCNVector3(x: position.x, y: position.y, z: position.z)
+    }
+    
+    func setTargetPosition(imageAnchorPosition: SCNVector3) {
+        let theta = calculateTheta(imageAnchorPosition: imageAnchorPosition)
+        
+        let targetOriginPosition = simd_float2(-2.0, 1.0)
+        let targetTranslatedVector = rotateVector(targetOriginPosition, angle: theta)
+        let targetPosition = SCNVector3(x: targetTranslatedVector.x, y: 0, z: -targetTranslatedVector.y)
+        targetNode.position = targetPosition
+    }
+    
+    func calculateTheta(imageAnchorPosition: SCNVector3) -> Double {
+        let imageVector = simd_float2(1, 1)
+        let arVector = simd_float2(imageAnchorPosition.x, imageAnchorPosition.z)
+        
+        let originVectorLength = simd_length(imageVector)
+        let arVectorLength = simd_length(arVector)
+        let adotb = imageVector.x * arVector.x + imageVector.y * arVector.y
+        let theta = Double(acos( adotb / (originVectorLength * arVectorLength)))
+        
+        return theta
+    }
+    
+    func rotateVector(_ vector: simd_float2, angle: Double) -> simd_float2 {
+        let angle = Measurement(value: angle, unit: UnitAngle.degrees)
+        let radians = Float(angle.converted(to: .radians).value)
+        
+        let rotationMatrix = makeRotationMatrix(angle: radians)
+        let rotatedVector = vector * rotationMatrix
+        
+        return rotatedVector
+    }
+    
+    /// - Parameter angle: in radians
+    func makeRotationMatrix(angle: Float) -> simd_float2x2 {
+        let rows = [
+            simd_float2(cos(angle), -sin(angle)),
+            simd_float2(sin(angle), cos(angle)),
+        ]
+        
+        return float2x2(rows: rows)
+    }
+    
 }
 
 extension ViewController: ARSCNViewDelegate {
@@ -74,5 +119,21 @@ extension ViewController: ARSCNViewDelegate {
 }
 
 extension ViewController: ARSessionDelegate {
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        if let imageAnchor = anchors.first as? ARImageAnchor {
+            let imageAnchorPosition = getPosition(from: imageAnchor.transform)
+            
+//            let node = SCNNode()
+//            let box = SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0)
+//            node.geometry = box
+//            node.position = imageAnchorPosition
+//            node.addChildNode(targetNode)
+            sceneView.scene.rootNode.addChildNode(targetNode)
+            setTargetPosition(imageAnchorPosition: imageAnchorPosition)
+            
+            sceneView.pointOfView?.addChildNode(arrowNode)
+        }
+    }
     
 }
